48d1740f9994e8bb69f6224e0092cf7450f723ab
Andres Narvaez <andres154nb@gmail.com>
2025-10-07 23:33:09 -0400

    Learning React - Final Project.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+import reducer from './CartSlice';
+
+
  const store = configureStore({
     reducer: {
         cart: cartReducer,
